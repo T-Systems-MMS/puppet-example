58f3f539cc966e6caf9cf142cd48729747fdf78e
Robert Heinzmann <Robert.Heinzmann@external.t-systems.com>
2017-03-21 13:09:41 +0100

    Cleanup Repo and add APACHE License

--- Puppetfile
+++ Puppetfile
@@ -4,42 +4,36 @@ forge "https://forge.puppetlabs.com"
 ### Forge Modules ###
 #####################
 
-moduledir 'forge'
-
 # Defaults and modules needed
 mod "puppetlabs/concat"
 mod "puppetlabs/stdlib"
-mod "crayfishx/firewalld"
 mod "puppetlabs/firewall"
 mod "puppetlabs/apache"
 mod "puppetlabs/mysql"
 mod "puppetlabs/tomcat"
-mod "puppet/stash"
 mod "puppetlabs/lvm"
 mod "puppetlabs/motd"
+mod "puppet/stash"
+mod "crayfishx/firewalld"
 
-# ACME dependencies
+# Organizational dependencies
 mod "treydock/gpg_key"
 
 ######################
 ### GIT Submodules ###
 ######################
 
-moduledir '3rdparty'
-
 #mod "r10k",
 #  :git => 'https://github.com/acidprime/r10k.git',
-#  :tag => 'v3.2.0'
+#  :ref => 'v3.2.0'
 
 
-#############################
-### ACME Specific Modules ###
-#############################
+#####################################
+### Organization Specific Modules ###
+#####################################
 
-moduledir 'acme'
-
-## repository etc.
-#mod "acme_puppet_base", 
-#  :git => "https://git.example.com/acme_puppet_base.git", 
-#  :ref => "development" 
+## Git Source
+#mod "mms_puppet_base",
+#  :git => "https://git.example.com/mms_puppet_base.git",
+#  :ref => "master"
 
--- localmodules/my_webserver/manifests/init.pp
+++ localmodules/my_webserver/manifests/init.pp
@@ -1,43 +1,44 @@
 class my_webserver (
-    $my_vhosts = {},
-) inherits stdlib {
+  $my_vhosts = {},
+) {
 
-    include apache
+  include stdlib
+  include apache
 
-    create_resources(apache::vhost, $my_vhosts)
+  ## Create the vhosts
+  create_resources(apache::vhost, $my_vhosts)
 
-    # My Content Files 
-    file { "/var/www/webmail/index.html": 
-       ensure => 'file',
-       content => "<html><head></head><body><h1> Welcome to my Webmail on ${::fqdn} ($::clientcert)</h1></body></html>",
-       owner => 'apache',
-       group => 'apache',
-       tag => 'myhomepages',
-    }
+  ## Some example content
+  file { "/var/www/webmail/index.html": 
+    ensure  => 'file',
+    content => "<html><head></head><body><h1> Welcome to my Webmail on ${::fqdn} ($::clientcert)</h1></body></html>",
+    owner   => 'apache',
+    group   => 'apache',
+    tag     => 'myhomepages',
+  }
 
-    file { "/var/www/html/index.html": 
-       ensure => 'file',
-       content => "<html><head></head><body><h1> Welcome to my Webpage on ${::fqdn} ($::clientcert)</h1></body></html>",
-       owner => 'apache',
-       group => 'apache',
-       tag => 'myhomepages',
-    }
+  file { "/var/www/html/index.html": 
+    ensure  => 'file',
+    content => "<html><head></head><body><h1> Welcome to my Webpage on ${::fqdn} ($::clientcert)</h1></body></html>",
+    owner   => 'apache',
+    group   => 'apache',
+    tag     => 'myhomepages',
+  }
 
-    file { "/var/www/blog/index.html": 
-       ensure => 'file',
-       content => "<html><head></head><body><h1> Welcome to my Blog on ${::fqdn} ($::clientcert)</h1></body></html>",
-       owner => 'apache',
-       group => 'apache',
-       tag => 'myhomepages',
-    }
+  file { "/var/www/blog/index.html": 
+    ensure  => 'file',
+    content => "<html><head></head><body><h1> Welcome to my Blog on ${::fqdn} ($::clientcert)</h1></body></html>",
+    owner   => 'apache',
+    group   => 'apache',
+    tag     => 'myhomepages',
+  }
 
-    # Dependency, vhosts first (creates the directory and then the content)
-    # Ordering in Puppet is "random" - so everything explicit please 
-    Apache::Vhost <| |> -> File <| tag == 'myhomepages' |>
+  # Dependency, vhosts first (creates the directory and then the content)
+  # Ordering in Puppet is "random" - so everything explicit please 
+  Apache::Vhost <| |> -> File <| tag == 'myhomepages' |>
 
 }
 
 # vim modeline - have 'set modeline' and 'syntax on' in your ~/.vimrc.
-# vi:syntax=puppet:filetype=puppet:ts=4:et:
-
+# vi:syntax=puppet:filetype=puppet:ts=2:et:
 
